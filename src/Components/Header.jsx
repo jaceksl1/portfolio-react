46f6336352220504a7a07c46b9acdc6331153dc5
Jacek <87030915+jaceksl1@users.noreply.github.com>
2021-11-03 14:59:20 +0100

    Add files via upload

---
+++ src/Components/Header.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+const Header = () => {
+  const element1 = <i class="fab fa-react fa-4x"></i>;
+  const element2 = <i class="fab fa-js-square fa-4x"></i>;
+  const element3 = <i class="fab fa-css3-alt fa-4x"></i>;
+  const element4 = <i class="fab fa-html5 fa-4x"></i>;
+  const element5 = <i class="fab fa-sass fa-3x"></i>;
+  const element6 = <i class="fab fa-bootstrap fa-4x"></i>;
+
+  return (
+    <nav>
+      <div className="nav-wrapper">
+        <h1 className="head-title">Hello, my name is Jacek.</h1>
+        <div className="subtitle">
+          <p>This is my portfolio!</p>
+          <div className="footer-wrapper">
+            <section className="gradient-section">
+              <ul className="gradient-ul">
+                <li>
+                  <p>{element1}</p>
+                </li>
+                <li>
+                  <p>{element2}</p>
+                </li>
+                <li>
+                  <p>{element3}</p>
+                </li>
+                <li>
+                  <p>{element4}</p>
+                </li>
+                <li>
+                  <p>{element5}</p>
+                </li>
+                <li>
+                  <p>{element6}</p>
+                </li>
+              </ul>
+            </section>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
