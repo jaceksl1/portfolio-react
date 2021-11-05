1ab0e242b9e490865ea805f43e36aadde1a6538b
Jacek <87030915+jaceksl1@users.noreply.github.com>
2021-11-05 12:32:21 +0100

    Add files via upload

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Footer = () => {
+  const element1 = <i class="fab fa-linkedin fa-4x"></i>;
+  const element2 = <i class="fab fa-github fa-4x"></i>;
+  const element3 = <i class="fas fa-envelope-open-text fa-4x"></i>;
+  const element4 = <i class="fab fa-codepen fa-4x"></i>;
+
+  return (
+    <footer className="footer-wrapper">
+      <h1 className="contact-section-header">
+        Below You can find my profiles and personal contact.
+      </h1>
+      <p>Is there anything I can help you with?</p>
+      <section className="gradient-section">
+        <ul className="gradient-ul">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/jacek-s%C5%82omian-759171b9/"
+              target="_blank"
+              rel="noreferrer">
+              {element1}
+            </a>
+          </li>
+          <li>
+            <a
+              id="profile-link"
+              href="https://github.com/jaceksl1"
+              target="_blank"
+              rel="noreferrer">
+              {element2}
+            </a>
+          </li>
+          <li>
+            <a href="mailto:j.k.slomian@gmail.com">{element3}</a>
+          </li>
+          <li>
+            <a
+              href="https://codepen.io/jaceksl1"
+              target="_blank"
+              rel="noreferrer">
+              {element4}
+            </a>
+          </li>
+        </ul>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
