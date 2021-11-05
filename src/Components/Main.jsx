a5c5c6bbc2b8335b9ec4098d13f47edbd5fdfca1
Jacek <87030915+jaceksl1@users.noreply.github.com>
2021-11-05 15:01:10 +0100

    Add files via upload

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <section className="section-wrapper">
+      <div className="main-container">
+        <div className="card">
+          <div className="circle">
+            <h2>1</h2>
+          </div>
+          <div className="content">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              similique magnam quo veritatis blanditiis modi rerum vitae saepe
+              ea? Numquam perferendis animi distinctio voluptatum earum
+              reiciendis quas unde corporis eveniet.
+            </p>
+            <a href="/#">See more</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="circle">
+            <h2>2</h2>
+          </div>
+          <div className="content">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              similique magnam quo veritatis blanditiis modi rerum vitae saepe
+              ea? Numquam perferendis animi distinctio voluptatum earum
+              reiciendis quas unde corporis eveniet.
+            </p>
+            <a href="/#">See more</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="circle">
+            <h2>3</h2>
+          </div>
+          <div className="content">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+              similique magnam quo veritatis blanditiis modi rerum vitae saepe
+              ea? Numquam perferendis animi distinctio voluptatum earum
+              reiciendis quas unde corporis eveniet.
+            </p>
+            <a href="/#">See more</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Main;
